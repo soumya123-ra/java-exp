59423ab99502531ca02689f58eb79cc5147b2859
soumya123-ra <soumyaranjan1957@gmail.com>
2026-06-03 17:05:41 +0530

    co

---
+++ EXPERIMENT6/Package1/Package1/Package1.java
@@ -0,0 +1,6 @@
+package Package1;
+public class Package1{
+    public void sayHello(){
+        System.out.println("Hello from Package1!");
+    }
+}
---
+++ EXPERIMENT6/Package1/Package1/Package2/Example.java
@@ -0,0 +1,16 @@
+package Package1;
+
+class Hello {
+    // Simple Hello class definition
+    void sayHello() {
+        System.out.println("Hello");
+    }
+}
+
+public class Example{
+    public static void main(String[] args){
+        Hello hello = new Hello();  
+        hello.sayHello();
+
+    }
+}
